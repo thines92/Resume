dc4876e7cda3ffad83b3fb50df4db8255a16b222
Tyler Hines <tylerhines1992@gmail.com>
2015-03-21 16:44:20 -0400

    Added int button

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -134,3 +134,15 @@
 
 		logClicks(x,y);
 	});
+
+	function inName(name) {
+		name = name.trim().split(" ");
+		console.log(name);
+		name[1] = name[1].toUpperCase();
+		name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+		return name[0] + " " + name[1];
+	}
+
+
+	$("#main").append(internationlizeButton);
